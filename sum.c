3dc1f9a249c27e70db94c7a5ad8b460d81912410
joaothompson <37004063+joaothompson@users.noreply.github.com>
2025-03-22 15:12:16 -0300

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
-int main() {    
-  int number1, number2, sum;
-  scanf("%d %d", &number1, &number2);
-
+int sum(number1, number2) {
+  int sum;
   // TODO: escreva uma expressao para armazenar number1 somado ao number2 e armazenar na variavel sum.
         
   // TODO: FIM - Edite apenas o intervalo do TODO  
-  printf("%d", sum);
-  return 0;
+  return sum;
 }
