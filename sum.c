e348de04478de4cfe22716c913c5493bd7a3e9e1
joaothompson <37004063+joaothompson@users.noreply.github.com>
2025-03-26 01:00:13 -0300

    Update and rename sum.c to student.c

--- sum.c
+++
@@ -1,8 +0,0 @@
-#include <sum.h>
-int sum(int number1, int number2) {
-  int sum;
-  // TODO: escreva uma expressao para armazenar number1 somado ao number2 e armazenar na variavel sum.
-        
-  // TODO: FIM - Edite apenas o intervalo do TODO  
-  return sum;
-}
