3ec00fd9752560414fca00531f3aa73e0154f81c
joaothompson <37004063+joaothompson@users.noreply.github.com>
2025-03-26 01:00:48 -0300

    Update test.c

--- test.c
+++ test.c
@@ -1,11 +1,11 @@
-#include <sum.h>
+#include <student.h>
 #include <stdio.h>
 
 int main(void)
 {
   int number1, number2, total;
   scanf("%d %d", &number1, &number2);
-  total = sum(number1, number2);
+  total = biggest(number1, number2);
   printf("%d\n", total);
   return 0;
 }
